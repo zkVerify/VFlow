ed94372a0e5d5b685b8141bc4bd938cf68ad0d9b
Luca Giussani <lucagiussani@horizenlabs.io>
2025-11-25 16:02:02 +0100

    Add deliveryFee method to xcm precompile

--- runtime/vflow/src/tests/xcm_teleport_integration.rs
+++ runtime/vflow/src/tests/xcm_teleport_integration.rs
@@ -1,10 +1,16 @@
 use crate::{
     configs::xcm::{NativeAssetId, RelayLocation},
-    currency::VFY,
+    constants::currency::VFY,
     tests::ALICE,
-    AccountId, Runtime, RuntimeOrigin, ZKVXcm,
+    AccountId, Runtime, RuntimeCall, RuntimeOrigin, ZKVXcm, U256,
 };
+use alloy::primitives::U256 as Uint256;
+use alloy_sol_types::{sol, SolCall, SolValue};
+use fp_evm::CallInfo;
+use fp_rpc::runtime_decl_for_ethereum_runtime_rpc_api::EthereumRuntimeRPCApiV5;
 use frame_support::assert_ok;
+use precompile_utils::precompile_set::AddressU64;
+use sp_core::Get;
 use sp_runtime::BuildStorage;
 use xcm::v5::{Asset, Assets, Fungibility, Junction, Location, WeightLimit};
 use xcm::{VersionedAssets, VersionedLocation};
@@ -76,4 +82,117 @@ fn can_teleport_vfy_to_relay() {
             WeightLimit::Unlimited
         ));
     });
+}
+
+#[test]
+fn xcm_teleport_precompile_delivery_fee_computation_is_correct() {
+    new_test_ext().execute_with(|| {
+        let from = ALICE;
+        let to = [0x42u8; 32];
+        let amount = 1 * VFY;
+
+        let fees_from_precompile = compute_teleport_delivery_fees_via_precompile(from, to, amount);
+        let fees_from_dry_run = compute_teleport_delivery_fees_via_dry_run(from, to, amount);
+
+        assert_eq!(fees_from_precompile, fees_from_dry_run);
+    });
+}
+
+fn compute_teleport_delivery_fees_via_precompile(
+    from: [u8; 20],
+    account: [u8; 32],
+    amount: u128,
+) -> u128 {
+    sol! {
+        contract IXcmTeleportPrecompile {
+            function deliveryFee(bytes32 id, uint256 amount) external returns (uint256);
+        }
+    }
+
+    let precompile_address = AddressU64::<2060>::get();
+    let calldata = IXcmTeleportPrecompile::deliveryFeeCall {
+        id: account.into(),
+        amount: Uint256::from(amount),
+    }
+    .abi_encode();
+
+    let CallInfo { value, .. } = Runtime::call(
+        from.into(),
+        precompile_address,
+        calldata,
+        U256::zero(),
+        U256::from(1_000_000),
+        None,
+        None,
+        None,
+        false,
+        None,
+    )
+    .unwrap();
+
+    Uint256::abi_decode(&value).unwrap().to()
+}
+
+fn compute_teleport_delivery_fees_via_dry_run(
+    from: [u8; 20],
+    account: [u8; 32],
+    amount: u128,
+) -> u128 {
+    let destination = VersionedLocation::V5(RelayLocation::get());
+
+    let beneficiary = VersionedLocation::V5(Location::new(
+        0,
+        [Junction::AccountId32 {
+            network: None,
+            id: account,
+        }],
+    ));
+
+    let assets = VersionedAssets::V5(Assets::from(vec![Asset {
+        id: NativeAssetId::get(),
+        fun: Fungibility::Fungible(amount),
+    }]));
+
+    let call = pallet_xcm::Call::<Runtime>::teleport_assets {
+        dest: Box::new(destination.clone()),
+        beneficiary: Box::new(beneficiary.clone()),
+        assets: Box::new(assets.clone()),
+        fee_asset_item: 0,
+    }
+    .into();
+
+    compute_delivery_fees_for_call(
+        RuntimeOrigin::signed(from.into()),
+        call,
+        destination.clone(),
+    )
+}
+
+fn compute_delivery_fees_for_call(
+    origin: RuntimeOrigin,
+    call: RuntimeCall,
+    destination: VersionedLocation,
+) -> u128 {
+    ZKVXcm::dry_run_call::<
+        Runtime,
+        <Runtime as pallet_xcm::Config>::XcmRouter,
+        <Runtime as pallet_xcm::Config>::RuntimeOrigin,
+        <Runtime as pallet_xcm::Config>::RuntimeCall,
+    >(origin, call, 5)
+    .unwrap()
+    .forwarded_xcms
+    .into_iter()
+    .filter(|(location, _xcms)| location == &destination)
+    .flat_map(|(_location, xcms)| xcms.into_iter())
+    .map(|xcm| -> Assets {
+        pallet_xcm::Pallet::<Runtime>::query_delivery_fees(destination.clone(), xcm)
+            .unwrap()
+            .try_into()
+            .unwrap()
+    })
+    .flat_map(|assets| assets.into_inner())
+    .fold(0, |acc, asset: Asset| match asset.fun {
+        Fungibility::Fungible(amount) => acc + amount,
+        Fungibility::NonFungible(_) => acc,
+    })
 }
